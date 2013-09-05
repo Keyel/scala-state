b63c3dc3cbec2c80beb5dcbb8dce96497b16874a
Keyel <keyel74@gmail.com>
2013-09-05 09:36:47 +0200

    evolved a bit

--- src/main/scala/keyel/state/StateTransition.scala
+++ src/main/scala/keyel/state/StateTransition.scala
@@ -2,44 +2,67 @@ package keyel.state
 
 import StateTransition._
 
-case class StateTransition[S, +A](trans: S => (S, A)) {
+case class StateTransition[S, +A](evolve: S => (S, A)) {
   def flatMap[B](f: A => StateTransition[S, B]): StateTransition[S, B] = StateTransition { s: S =>
-    val (s2, a) = trans(s)
-    f(a).trans(s2)
+    val (s2, a) = evolve(s)
+    f(a).evolve(s2)
   }
 
+  //a mdified statetransition, changing the result without further state evolution
   def map[B](f: A => B): StateTransition[S, B] = 
     flatMap { a => unit(f(a)) }
   
+  //evolves a state with two kind of stateTransition, getting the intermediate and final result, and applying a binary function to them
   def mapbin[B, C] ( st : StateTransition[S, B] )(f: (A,B) => C): StateTransition[S, C] =
     flatMap { a => st.map { b => f(a,b) } }
 
   def mapbin_viaFor[B, C] ( st : StateTransition[S, B] )(f: (A,B) => C): StateTransition[S, C] =
     for{
-      a <- this  //a bejovo s-et transzformalja (s2, a) -ra , es kiveszi belole az a-t
-      b <- st
-    } yield f(a,b)
+      a <- this  //evolve by this, get the result from it to a
+      b <- st    //evolve by st, get the result to b
+    } yield f(a,b)  //the combined evolutions result will be mapped to f(a,b)
   
 }
 
 object StateTransition {
+  //returns a unit state transition, with result a
   def unit[S, A](a: A): StateTransition[S, A] = StateTransition { s => (s, a) }
 
+  //returns a state transition which is equivalent with $transition applied $count times
   def repeat[S, A](transition: StateTransition[S, A], count: Int): StateTransition[S, A] = StateTransition { s =>
-    val (newState, a) = transition.trans(s)
+    val (newState, a) = transition.evolve(s)
     if (count == 1)
       (newState, a)
     else
-      repeat(transition, count - 1).trans(newState)
+      repeat(transition, count - 1).evolve(newState)
   }
 
-  //eloszor az utolso transitiont hajtjuk vegre
+  //applying transitions from right to left, forgetting intermediate results
   def compose[S, A](transitions: List[StateTransition[S, A]]): StateTransition[S, A] = transitions.reduceRight { (t1, t2) =>
-    StateTransition { s =>
-      val (s2, _) = t2.trans(s)
-      t1.trans(s2)
-    }
+    t2.mapbin(t1) { (a2, a1) => a1 }
   }
 
+  //applying transitions from right to left, collecting the intermediate results
+  def sequence[S, A](transitions: List[StateTransition[S, A]]) : StateTransition[S, List[A]] = transitions.foldRight(unit[S,List[A]](List[A]())) { (t, acc) =>
+    t.mapbin(acc) { (a, as) => a :: as }
+  }
+
+  def repeatGetAll[S,A](transition: StateTransition[S,A], count: Int) : StateTransition[S, List[A]] =
+    sequence(List.fill(count)(transition))
+
+
+  def modify[S](f: S => S): StateTransition[S, Unit] = for {
+    s <- get // Gets the current state and assigns it to `s`.
+    _ <- set(f(s)) // Sets the new state to `f` applied to `s`.
+  } yield ()
+
+  //get returns a stateTransition, which result is the state itself, without changing the state
+  def get[S] : StateTransition[S,S] = StateTransition { s: S =>
+    (s, s)
+  }
+
+  def set[S](s: S) : StateTransition[S,Unit] = StateTransition{ _ => 
+    (s, ())
+  }
 }
 
--- src/test/scala/keyel/state/StateTransitionTestSuite.scala
+++ src/test/scala/keyel/state/StateTransitionTestSuite.scala
@@ -21,27 +21,69 @@ class StateTransitionTestSuite extends FlatSpec with ShouldMatchers {
 	  (IntState(intState.n + 1), intState.n + 1)
 	}
 	
-	val intstate_incrementAndDouble : StateTransition[IntState, Int] = intstate_incrementer map { _ * 2 }
+	val intstate_incrementReturnDouble : StateTransition[IntState, Int] = intstate_incrementer map { _ * 2 }
+
+	val intstate_doubler : StateTransition[IntState, Int] = StateTransition { intState => 
+		(IntState(intState.n * 2), intState.n * 2)
+	}
   
 	"state transition twice" should "work" in {
-	  val (onest, onezero) = intstate_incrementer.trans(zerost) 
-	  val (twost, two) = intstate_incrementer.trans(onest)
+	  val (onest, onezero) = intstate_incrementer.evolve(zerost) 
+	  val (twost, two) = intstate_incrementer.evolve(onest)
 	  (two) should equal(2)
 	}
 	
 	"state transition repeat" should "work" in {
 	  val incrementThreeTimes = repeat(intstate_incrementer, 3)
 	  
-	  val (_, three) = incrementThreeTimes.trans(zerost)
+	  val (IntState(n), three) = incrementThreeTimes.evolve(zerost)
 	  three should equal(3)
+	  n should equal(3)
 	}
 	
-	"state transition map" should "work" in {
-	  val (_, two) = intstate_incrementAndDouble.trans(zerost) 
+	"state transition map" should "gives the modified answer, without changing the state" in {
+	  val (IntState(n), two) = (intstate_incrementer map { _ * 2 }).evolve(zerost) 
 	  two should equal(2)
+	  n should equal(1)
 	}
 	
-	"mapbin" should "work" in {
-	  
+	"mapbin" should "evolve the state twice, and use the two results" in {
+		val sttr = intstate_incrementer.mapbin(intstate_doubler) { (_,_) }
+		val (IntState(n), r) = sttr.evolve(zerost)
+		r should equal( (1,2) )
+		n should equal( 2 )
+	}
+
+	"compose" should "return a StateTransition, which is equivalent of applying all the StateTransitions from right to left" in {
+		val sttr = compose(List(intstate_doubler, intstate_incrementer, intstate_incrementer))
+		val (IntState(n), r) = sttr.evolve(zerost)
+		r should equal( 4 )
+		n should equal( 4 )
+	}
+
+	"sequence" should "return the intermediate results also" in {
+		def unitis = unit[IntState, Int] _
+		val sttr = sequence(List(unitis(1), unitis(2), unitis(3)))
+		val (IntState(n), r) = sttr.evolve(zerost)
+		n should equal(0) //unit does not change state
+		r should equal(List(1,2,3))
+	}
+
+	"repeatGetAll" should "return the list of the intermediate results of repeating the StateTransition" in {
+		val sttr = repeatGetAll(intstate_incrementer, 3)
+		val (IntState(n), r) = sttr.evolve(zerost)
+		n should equal(3)
+		r should equal(List(1,2,3))
+	}
+
+	"modify" should "work like an imperative assignment" in {
+		def increment(s: IntState) : IntState = { 
+			val IntState(n) = s
+			IntState(n + 1) 
+		}
+		
+		val (IntState(n), r) = modify(increment).evolve(zerost)
+		n should equal(1)
+		r should equal(())
 	}
 }
